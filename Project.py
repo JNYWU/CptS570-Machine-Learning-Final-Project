fb703b2369f83febc78ca82af024a9219d3dec59
Chiu-Yuan Wu <johnnywu203@gmail.com>
2021-12-14 16:57:22 -0800

    Decision Tree

--- Project.py
+++ Project.py
@@ -3,10 +3,32 @@
 import DataPreprocess
 import numpy as np
 import pandas as pd
+import sklearn
+from sklearn.metrics import accuracy_score
+from matplotlib import pyplot as plt
     
 data = DataPreprocess.ReadData()
 
 X_train, y_train, X_test, y_test = DataPreprocess.SplitData(data)
 
+def DecisionTree():
+    from sklearn.tree import DecisionTreeClassifier
+    
+    clf = DecisionTreeClassifier()
+    clf.fit(X_train, y_train)
+    
+    predict = clf.predict(X_test)
+    accuracy = accuracy_score(predict, y_test)
+    
+    importance = clf.feature_importances_
+    
+    return accuracy, importance
 
+if __name__ == "__main__":
+    print("Happy Machine Learning")
+    
+    # decision tree
+    decisionTreeAccuracy, decisionTreeImportance = DecisionTree()
+    print("Decision Tree Accuracy: ", decisionTreeAccuracy)
+    plt.bar([x for x in range(len(decisionTreeImportance))], decisionTreeImportance)
 # %%
