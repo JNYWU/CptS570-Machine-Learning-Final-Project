26975f62c8639a453e0eefda3fd190361185f2f0
zheng0258 <guang-zheng.lee@wsu.edu>
2021-12-14 17:42:21 -0800

    nb, lr

--- Project.py
+++ Project.py
@@ -6,7 +6,12 @@ import pandas as pd
 import sklearn
 from sklearn.metrics import accuracy_score
 from matplotlib import pyplot as plt
-    
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.inspection import permutation_importance
+
+
+
 data = DataPreprocess.ReadData()
 
 X_train, y_train, X_test, y_test = DataPreprocess.SplitData(data)
@@ -20,15 +25,57 @@ def DecisionTree():
     predict = clf.predict(X_test)
     accuracy = accuracy_score(predict, y_test)
     
-    importance = clf.feature_importances_
+    importance = permutation_importance(clf, X_train, y_train, n_repeats=10)
     
-    return accuracy, importance
+    return accuracy, importance.importances_mean
+
+def NaiveBayes():
+
+    clf = GaussianNB()
+    clf.fit(X_train, y_train)
+
+    predict = clf.predict(X_test)
+
+    importance = permutation_importance(clf, X_train, y_train, n_repeats=10)
+
+    #new_importance = importance.importances_mean - min(importance.importances_mean)
+    # get the accuracy score
+    accuracy = accuracy_score(predict, y_test)
+    
+    return accuracy, importance.importances_mean
+
+def LogisticRegression():
+    from sklearn.linear_model import LogisticRegression
+    
+    # fit the model
+    clf = LogisticRegression()
+    clf.fit(X_train, y_train)
+
+    # get accuracy score
+    predict = clf.predict(X_test)
+
+    importance = permutation_importance(clf, X_train, y_train, n_repeats=10)
+    accuracy = accuracy_score(predict, y_test)
+
+    return accuracy, importance.importances_mean
 
 if __name__ == "__main__":
     print("Happy Machine Learning")
     
-    # decision tree
-    decisionTreeAccuracy, decisionTreeImportance = DecisionTree()
-    print("Decision Tree Accuracy: ", decisionTreeAccuracy)
-    plt.bar([x for x in range(len(decisionTreeImportance))], decisionTreeImportance)
+    # # decision tree
+    # decisionTreeAccuracy, decisionTreeImportance = DecisionTree()
+    # print("Decision Tree Accuracy: ", decisionTreeAccuracy)
+    # plt.bar([x for x in range(len(decisionTreeImportance))], decisionTreeImportance)
+
+    # # naive Bayes
+    # naiveBayesAccuracy, naiveBayesImportance = NaiveBayes()
+    # print("Naive Bayes Accuracy: ", naiveBayesAccuracy)
+    # plt.bar([x for x in range(len(naiveBayesImportance))], naiveBayesImportance)
+    # plt.show()
+
+    # LogisticRegression
+    logisticRegressionAccuracy, logisticRegressionImportance = NaiveBayes()
+    print("Naive Bayes Accuracy: ", logisticRegressionAccuracy)
+    plt.bar([x for x in range(len(logisticRegressionImportance))], logisticRegressionImportance)
+    plt.show()
 # %%
